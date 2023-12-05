83cd2198cbf86530d9e2cefe7516e7c09e14a62b
palminschi <mihaipalminschi@gmail.com>
2023-12-05 21:56:54 +0100

    Bloc implemented

--- lib/features/news/domain/usecases/get_article_usecase.dart
+++ lib/features/news/domain/usecases/get_article_usecase.dart
@@ -3,11 +3,11 @@ import 'package:clean_news_app/core/usecases/usecase.dart';
 import 'package:clean_news_app/features/news/domain/entities/article.dart';
 import 'package:clean_news_app/features/news/domain/repository/article_repository.dart';
 
-class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>, void> {
+class GetArticlesUseCase implements UseCase<DataState<List<ArticleEntity>>, void> {
 
   final ArticleRepository _articleRepository;
 
-  GetArticleUseCase(this._articleRepository);
+  GetArticlesUseCase(this._articleRepository);
 
   @override
   Future<DataState<List<ArticleEntity>>> call({void params}) {
---
+++ lib/features/news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:clean_news_app/core/resources/data_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:clean_news_app/features/news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:clean_news_app/features/news/presentation/bloc/article/remote/remote_article_state.dart';
+
+import '../../../../domain/usecases/get_article_usecase.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent,RemoteArticlesState> {
+
+  final GetArticlesUseCase _getArticlesUseCase;
+  RemoteArticlesBloc(this._getArticlesUseCase): super(const RemoteArticlesLoading()) {
+    on <GetArticles> (onGetArticles);
+  }
+
+  void onGetArticles(GetArticles event, Emitter<RemoteArticlesState> emitter) async {
+    final dataState = await _getArticlesUseCase();
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(
+        RemoteArticlesDone(dataState.data!)
+      );
+    }
+
+    if (dataState is DataFailed) {
+      emit(
+        RemoteArticlesError(dataState.error!)
+      );
+    }
+  }
+}
